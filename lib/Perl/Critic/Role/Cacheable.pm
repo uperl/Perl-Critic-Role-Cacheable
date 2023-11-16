21f0cedecd997339b7c7116dc10bf00b227946fb
Graham Ollis <plicease@cpan.org>
2023-11-16 05:19:21 -0700

    Initial structure

---
+++ lib/Perl/Critic/Role/Cacheable.pm
@@ -0,0 +1,11 @@
+use warnings;
+use 5.020;
+use experimental qw( postderef signatures );
+
+package Perl::Critic::Role::Cacheable {
+
+  # ABSTRACT: Add cacheing to a Perl::Critic subclass
+}
+
+1;
+
---
+++ t/perl_critic_role_cacheable.t
@@ -0,0 +1,8 @@
+use Test2::V0 -no_srand => 1;
+use Perl::Critic::Role::Cacheable;
+
+ok 1, 'todo';
+
+done_testing;
+
+
